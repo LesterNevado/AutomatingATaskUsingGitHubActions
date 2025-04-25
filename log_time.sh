3dcc71b636cc2b3e8407f62fadaa79c9fa51caea
LesterNevado <152840417+LesterNevado@users.noreply.github.com>
2025-04-25 22:33:18 +0800

    Update log_time.sh

--- log_time.sh
+++ log_time.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "[$(date)] - Logged by GitHub user: LesterNevado" >> log.txt
 count=$(wc -l < log.txt)
-echo "The log file has been updated $count times."
+echo "The log file has been updated $count times." >> log.txt
